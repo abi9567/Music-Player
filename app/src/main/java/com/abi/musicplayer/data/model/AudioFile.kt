1cdfd7a01c2cd18b40987f704c812cc8bcea73ac
abi <abicherayakkattu2012@gmail.com>
2026-02-20 00:01:22 +0530

    data cass

---
+++ app/src/main/java/com/abi/musicplayer/data/model/AudioFile.kt
@@ -0,0 +1,9 @@
+package com.abi.musicplayer.data.model
+
+data class AudioFile(
+    val fileName: String,
+    val thumbnail: String,
+    val artistName: String,
+    val totalDuration: String
+)
+
---
+++ app/src/main/java/com/abi/musicplayer/ui/screens/MusicPlayerScreen.kt
@@ -0,0 +1,2 @@
+package com.abi.musicplayer.ui.screens
+
